deef9e071576c59e811260742fa3e5895c0f5c07
Yahya Shaker <yahya.shaker@gmail.com>
2023-08-16 19:14:29 -0400

    push

--- providers.tf
+++ providers.tf
@@ -1,13 +1,8 @@
 rovider "azurerm" {
-
     subscription_id = var.general.subscription_id
-
     tenant_id       = var.general.tenant_id
-
     skip_provider_registration = true
-
     features {}
-
 }
 
  
